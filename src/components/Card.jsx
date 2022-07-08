30ad78b59f3b1b9acbf4f8cbfd78e18e1db6a72d
Raymond Joseph Cañete <raymondcanete1997@gmail.com>
2022-07-08 23:28:36 +0800

    Implement CSS/UI and Fetchin data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import "../styles/cards.css";
 
-export default function Card({ data }) {
+export default function Card({ hackerList }) {
+  const removeSpecialCharacter = (word) => {
+    return word.replace(/[^a-z]/g);
+  };
+
   return (
     <>
-      <h1>{data}</h1>
+      {hackerList.map((hacker) => (
+        <div className="card" key={hacker.id}>
+          <div className="card__header">
+            <img
+              src="https://source.unsplash.com/600x400/?computer"
+              alt="card__image"
+              className="card__image"
+              width="600"
+            />
+          </div>
+          <div className="card__body">
+            <span className="tag tag-blue">{hacker.type.toUpperCase()}</span>
+            <h4>{hacker.title ? hacker.title : "Title not found"}</h4>
+            <p>{hacker.text}</p>
+          </div>
+          <div className="card__footer">
+            <div className="user">
+              <img
+                src="/assets/anonymouse.png"
+                alt="user__image"
+                className="user__image"
+              />
+              <div className="user__info">
+                <h5>{hacker.by ? hacker.by : "Unable to get user"}</h5>
+                <small>
+                  Story Timestamped: <strong>{hacker.time}</strong>
+                </small>
+                <br />
+                <small>
+                  Score:{` `}
+                  <strong
+                    className={
+                      hacker.score ? "score__existing" : "score__not_found"
+                    }
+                  >
+                    {hacker.score ? hacker.score : "N/A"}
+                  </strong>
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </>
   );
 }
